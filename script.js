41ca81f3e3b1520ffabc004eab31a8fd4c061f2c
Nephew20 <cofieldk20@gmail.com>
2023-02-08 23:01:34 -0500

    created prompts for the user

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-console.log(generateBtn)
+var specialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', "`", '{', '|', "'", '}', '~', ' '];
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var choice = window.confirm("Would you like to generate a password?");
+  
+  if (choice == false) {
+    alert("Click the Generate Password Button Again");
+  } else if (choice == true) {
+    var userChoice = prompt("Choose how many characters you would like? Choose between 8-128");
+  
+    if (userChoice < 8) {
+      prompt('Not Enough Characters. Choose between 8 - 128');
+    } else if (userChoice > 129) {
+      prompt('Too many Characters. Choose between 8 - 128');
+    }   
+
+    console.log(userChoice)
+  }  
+  
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
