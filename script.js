4ea7590a1aaf812f9fb6afd38c3a405710622ba6
Nephew20 <cofieldk20@gmail.com>
2023-02-11 18:30:35 -0500

    generated random characters

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', "`", '{', '|', "'", '}', '~', ' '];
+var Char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var SpecialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', "`", '{', '|', "'", '}', '~', ' '];
+var Num = ['1','2','3','4','5','6','7','8','9','0']
 
 // Write password to the #password input
 function writePassword() {
   var choice = window.confirm("Would you like to generate a password?");
-  
+   
   if (choice == false) {
     alert("Click the Generate Password Button Again");
   } else if (choice == true) {
-    var userChoice = prompt("Choose how many characters you would like? Choose between 8-128");
+    var choiceChar = prompt("Choose how many characters you would like? Choose between 8-128");
+    
+    // Making sure that the user inputs choice of 8-128 characters
+    if (choiceChar < 8) {
+      alert('Not Enough Characters. Choose between 8 - 128');
+      writePassword();
+    } else if (choiceChar > 129) {
+      alert('Too many Characters. Choose between 8 - 128');
+      writePassword();
+    }
+  }
   
-    if (userChoice < 8) {
-      prompt('Not Enough Characters. Choose between 8 - 128');
-    } else if (userChoice > 129) {
-      prompt('Too many Characters. Choose between 8 - 128');
-    }   
+  function generatePassword() {
+    var emptyPass = []
+    randoomChar = Char[Math.floor(Math.random()*Char.length)];
+    randoomSpecChar = SpecialChar[Math.floor(Math.random()*SpecialChar.length)];
+    randoomNum = Num[Math.floor(Math.random()*Num.length)];
 
-    console.log(userChoice)
-  }  
-  
-  // var password = generatePassword();
+    return randoomChar
+    // for (let x = 0; x <= userChar; x++) {
+      
+      
+    //   return randoomChar;
+    // }
+    
+  }
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
